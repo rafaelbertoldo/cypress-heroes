22f6f9c8b7a1a1796bae4dda6becdb29e24804c2
Rafael Bertoldo <chicobertoldo@gmail.com>
2025-06-19 07:03:14 -0300

    fix: add Cypress support to ESLint configuration

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,10 +1,16 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    'cypress/globals': true,
+    mocha: true  
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:cypress/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
---
+++ client/cypress/e2e/login.spec.cy.js
@@ -0,0 +1,5 @@
+describe("Login tests", () => {
+  it("passes", () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
