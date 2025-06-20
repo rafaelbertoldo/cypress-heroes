160826ca0b13944293ce647df5af71479b122b2d
Rafael Bertoldo <chicobertoldo@gmail.com>
2025-06-20 13:44:53 -0300

    first commit

---
+++ client/cypress/e2e/adminPermis.spec.cy.js
@@ -0,0 +1,39 @@
+var Chance = require('chance');
+
+var chance = new Chance();
+
+describe("Test admin permissions", () => {
+  it.skip("'Create new hero' button should be available ", () => {
+    const user = 'admin@test.com'
+    const password = 'test123'
+    cy.visit('http://localhost:3000/heroes')
+    cy.get('button').eq(0).click()
+    cy.get("[name='email']").type(user)
+    cy.get("[name='password']").type(password)
+    cy.get(".text-white").click()
+    cy.get("[href='/heroes/new']")
+  })
+
+  it.skip("Admin should be able to create a new hero", () => {
+    const user = 'admin@test.com'
+    const password = 'test123'
+    const newHero = chance.word({ syllables: 3 })
+    const price = chance.natural({ min: 10, max: 200 })
+    const fans = chance.natural({ min: 10, max: 30 })
+    const saves = chance.natural({ min: 1, max: 110 })
+    const randomPower = chance.natural({ min: 0, max: 7 })
+    cy.visit('http://localhost:3000/heroes')
+    cy.get('button').eq(0).click()
+    cy.get("[name='email']").type(user)
+    cy.get("[name='password']").type(password)
+    cy.get(".text-white").click()
+    cy.get("[href='/heroes/new']").click();
+    cy.get("[data-cy='nameInput']").type(newHero)
+    cy.get("[data-cy='priceInput']").type(price)
+    cy.get("[data-cy='fansInput']").type(fans)
+    cy.get("[data-cy='savesInput']").type(saves)
+    cy.get("option").eq(randomPower).click()
+    cy.get("[data-cy='avatarFile']").click()
+    
+  })
+})
--- client/cypress/e2e/login.spec.cy.js
+++ client/cypress/e2e/login.spec.cy.js
@@ -1,5 +1,22 @@
 describe("Login tests", () => {
-  it("passes", () => {
-    cy.visit('https://example.cypress.io')
+  it.skip("Login with admin valid credentials", () => {
+    const user = 'admin@test.com'
+    const password = 'test123'
+    cy.visit('http://localhost:3000/heroes')
+    cy.get('button').eq(0).click()
+    cy.get("[name='email']").type(user)
+    cy.get("[name='password']").type(password)
+    cy.get(".text-white").click()
+  })
+
+  it.skip("Login with invalid credentials", () => {
+    const user = 'test_123@test.com'
+    const password = 'test123_test'
+    cy.visit('http://localhost:3000/heroes')
+    cy.get('button').eq(0).click()
+    cy.get("[name='email']").type(user)
+    cy.get("[name='password']").type(password)
+    cy.get(".text-white").click()
+    cy.get(".text-red-500")
   })
 })
